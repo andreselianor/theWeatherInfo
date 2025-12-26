ceda3aa1956a5ef2eb94d5157f256bbdec823724
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-12-26 17:33:13 +0100

    feat: first commit / initial main structure of the website / foldering

---
+++ js/weatherInfo.js
@@ -0,0 +1,15 @@
+var content = document.getElementById("content");
+var URL = "https://api.open-meteo.com/v1/forecast?latitude=38&longitude=0.48&hourly=temperature_2m";
+
+function getWeather() {
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => showData(data))
+}
+
+function showData(data) {
+    console.log(data);
+    content.innerHTML = data.hourly.temperature_2m;
+}
+
+getWeather();
