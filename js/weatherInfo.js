2bbb79ef210389d30fee381d0c3c0d280bbb8396
andreselianor <113841548+andreselianor@users.noreply.github.com>
2026-01-03 01:09:40 +0100

    feat: added bakcground image / css styles color and size font / minor fixes

--- js/weatherInfo.js
+++ js/weatherInfo.js
@@ -35,10 +35,43 @@ function showData(data) {
         <p class="cardInfo">precipitation</p>
     `
     if (data.daily.weather_code[0] != null) {
+        displayWeatherCode(data.daily.weather_code[0]);
         node3.innerHTML =
             `<p class="cardData"><img class="logo" src="../media/icons/icon-sun.svg"></p>
         <p class="cardInfo">weather code</p>
     `
     }
 }
+function displayWeatherCode(code) {
+    if (code == 0 || code == 1 || code == 2)
+        return "icon-sun.svg";
+    if (code == 3)
+        return "icon-cloudy.svg";
+    if (code == 61 || code == 63 || code == 65)
+        return "icon-rain.svg";
+    if (code == 81 || code == 81 || code == 82)
+        return "icon-rain-showers.svg";
+    if (code == 71 || code == 73 || code == 75)
+        return "icon-snow.svg";
+    if (code == 45 || code == 48 || code == 75)
+        return "icon-fog.svg";
+    return "icon-sun.svg";
+}
+
 getWeather();
+
+/*
+0	Clear sky
+1, 2, 3	Mainly clear, partly cloudy, and overcast
+45, 48	Fog and depositing rime fog
+51, 53, 55	Drizzle: Light, moderate, and dense intensity
+56, 57	Freezing Drizzle: Light and dense intensity
+61, 63, 65	Rain: Slight, moderate and heavy intensity
+66, 67	Freezing Rain: Light and heavy intensity
+71, 73, 75	Snow fall: Slight, moderate, and heavy intensity
+77	Snow grains
+80, 81, 82	Rain showers: Slight, moderate, and violent
+85, 86	Snow showers slight and heavy
+95 *	Thunderstorm: Slight or moderate
+96, 99 *	Thunderstorm with slight and heavy hail
+*/
