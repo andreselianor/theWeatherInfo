ef15d34270b85960805faa72a44efd8b004e97c8
andreselianor <113841548+andreselianor@users.noreply.github.com>
2026-01-09 13:21:48 +0100

    feat: manage alter files / folder main structure of the webapp

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 class MyFooter extends HTMLElement {
     connectedCallback() {
-        fetch("./html/footer.html")
+        fetch("../html/footer.html")
             .then(response => response.text())
             .then(html => this.innerHTML = html)
     }
--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 class MyHeader extends HTMLElement {
     connectedCallback() {
-        fetch("./html/header.html")
+        fetch("../html/header.html")
             .then(response => response.text())
             .then(html => this.innerHTML = html)
     }
